7404a406dae7fdad716a26b258fe3c16b13ada8e
komfiGolabi <komfalos1@gmail.com>
2022-01-19 12:55:11 +0000

    created individual product template

--- products/views.py
+++ products/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
 
 
 def all_products(request):
-    """ A view to show all products,sorting and search queries """
+    """ A view for all products,sorting and search queries """
 
     products = Product.objects.all()
 
@@ -14,3 +14,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_single(request, product_id):
+    """ view showing  individual product """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_single.html', context)
