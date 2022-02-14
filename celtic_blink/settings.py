fb55ea843dcf1f10ff8d54e47c74afa77eec9f05
komfiGolabi <komfalos1@gmail.com>
2022-02-14 15:20:36 +0000

    fixed typo in localhost

--- celtic_blink/settings.py
+++ celtic_blink/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure--uzno4kmc#4t589*5k@oq=5m$&153e8v6h8s-o!dfefkfihh1-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['celtic-blink-ms3.herokuapp.com', 'loacalhost']
+ALLOWED_HOSTS = ['celtic-blink-ms3.herokuapp.com', 'localhost']
 
 
 # Application definition
