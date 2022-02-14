6c0454f38a211d631c30300ddc357b6ac9d944cf
komfiGolabi <komfalos1@gmail.com>
2022-02-14 14:52:55 +0000

    made some changes in database and allowed hosts settings

--- celtic_blink/settings.py
+++ celtic_blink/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure--uzno4kmc#4t589*5k@oq=5m$&153e8v6h8s-o!dfefkfihh1-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['celtic-blink-ms3.herokuapp.com', 'loacalhost']
 
 
 # Application definition
@@ -121,14 +121,19 @@ WSGI_APPLICATION = 'celtic_blink.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
