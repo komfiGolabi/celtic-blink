cf74cb43bd3861191110463176b4397e13b744b7
komfiGolabi <komfalos1@gmail.com>
2022-02-14 14:29:40 +0000

    created database fixtures and connected database to heroku one

--- celtic_blink/settings.py
+++ celtic_blink/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,7 +126,7 @@ WSGI_APPLICATION = 'celtic_blink.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
