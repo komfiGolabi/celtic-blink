ef2bc677406cdc7ce0923e4c5927bfe35c015894
komfiGolabi <komfalos1@gmail.com>
2022-02-15 16:37:40 +0000

    adding settings for sending emails

--- celtic_blink/settings.py
+++ celtic_blink/settings.py
@@ -213,3 +213,16 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'komfalos1@gmail.com'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'komfalos1@gmail.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
