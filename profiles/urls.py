460466a880306c54368097181b4a50d803ae96d9
komfiGolabi <komfalos1@gmail.com>
2022-03-04 18:51:16 +0000

    fixed issues with footer and error in a template

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('order_history/<order_number>', views.order_history, name='order_history'),
-    path('favourites/<int:product_id>>', views.favourite_add, name='favourite_add'),
+    path('favourite/<int:product_id>>', views.favourite_add, name='favourite_add'),
     path('profile/favourite', views.favourite_list, name='favourite_list')
 ]
